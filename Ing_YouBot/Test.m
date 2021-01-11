5fce48cd6c0ffc68579f48910d9cc2674b8953f2
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-11 17:23:36 +0100

    added Winkelbegrenzung for pickup

--- Ing_YouBot/Test.m
+++ Ing_YouBot/Test.m
@@ -4,13 +4,21 @@ GelenkPos(runROS(),[deg2rad(15) 0 0 deg2rad(15) pi]);
 %Klotzposition bestimmen und anfahren
 Pos=Klotz_Position;
 Winkel_1=Inverskinematik_fertig([Pos(1) Pos(2) Pos(3)+10 -pi/2 0]);
-GelenkPos(runROS(),Winkel_1);
+if Winkelbegrenzung(Winkel_1)
+    GelenkPos(runROS(),Winkel_1);
 
-%Greifen
-GreiferPos(runROS(), 20);
-Winkel_2=Inverskinematik_fertig([Pos(1) Pos(2) Pos(3)-20 -pi/2 0]);
-GelenkPos(runROS(),Winkel_2);
-GreiferPos(runROS(), 0);
-GelenkPos(runROS(),Winkel_1);
-GelenkPos(runROS(),Winkel_2);
-GreiferPos(runROS(), 20);
+    %Greifen
+    GreiferPos(runROS(), 20);
+    Winkel_2=Inverskinematik_fertig([Pos(1) Pos(2) Pos(3)-20 -pi/2 0]);
+    if Winkelbegrenzung(Winkel_2)
+        GelenkPos(runROS(),Winkel_2);
+        GreiferPos(runROS(), 0);
+        GelenkPos(runROS(),Winkel_1);
+        GelenkPos(runROS(),Winkel_2);
+        GreiferPos(runROS(), 20);
+    else
+        error('Winkelbegrenzung überschritten');
+    end
+else
+    error('Winkelbegrenzung überschritten');
+end
