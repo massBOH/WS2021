9f1d56f8ca0b4c42f47e68ce7573d0e01a8d827f
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-11 17:08:33 +0100

    update for theta1 > 170°

--- Ing_YouBot/Inverskinematik_fertig.m
+++ Ing_YouBot/Inverskinematik_fertig.m
@@ -24,34 +24,60 @@ a_3=135;
 a_4=217.5;
 d_1=33;
 
-%benötigte Werte
+theta_1=atan2(y,x);
 d=sqrt(x^2+y^2);
-
-r_d=d-d_1;
-
 z_d=z-a_1;
 
-theta_1=atan2(y,x);
+if(abs(theta_1)<deg2rad(170))
+    
+    %benötigte Werte
+    r_d=d-d_1;
+    
+    r_4=r_d-a_4*cos(psi);
 
-r_4=r_d-a_4*cos(psi);
+    z_4=z_d-a_4*sin(psi);
 
-z_4=z_d-a_4*sin(psi);
+    s=sqrt(r_4^2+z_4^2);
 
-s=sqrt(r_4^2+z_4^2);
+    alpha=atan2(z_4,r_4);
 
-alpha=atan2(z_4,r_4);
+    beta=acos((-s^2+a_2^2+a_3^2)/(2*a_2*a_3));
 
-beta=acos((-s^2+a_2^2+a_3^2)/(2*a_2*a_3));
+    %Berechnung der Winkel
+    theta_3=-(beta-pi);
+    theta_2=pi/2 -(alpha+ asin((sin(beta)*a_3)/s));
+    %psi = 0 = -theta2-theata3-theta4+pi/2
+    theta_4=-(psi+theta_2+theta_3-(pi/2));
+    theta_5=Koordinaten(5);
 
-%Berechnung der Winkel
-theta_3=-(beta-pi);
-theta_2=pi/2 -(alpha+ asin((sin(beta)*a_3)/s));
-%psi = 0 = -theta2-theata3-theta4+pi/2
-theta_4=-(psi+theta_2+theta_3-(pi/2));
-theta_5=Koordinaten(5);
-% theta_2=rad2deg(theta_2);
-% theta_3=rad2deg(theta_3);
-% theta_4=rad2deg(theta_4);
+else
+    %benötigte Werte
+    r_d=d+d_1;
+    if(theta_1>0)
+        theta_1=theta_1-pi;
+    else
+        theta_1=theta_1+pi;
+    end
 
+    r_4=r_d-a_4*cos(psi);
 
+    z_4=z_d-a_4*sin(psi);
+
+    s=sqrt(r_4^2+z_4^2);
+
+    alpha=atan2(z_4,r_4);
+
+    beta=acos((-s^2+a_2^2+a_3^2)/(2*a_2*a_3));
+
+    %Berechnung der Winkel
+    theta_3=-(beta-pi);
+    theta_2=pi/2 -(alpha+ asin((sin(beta)*a_3)/s));
+    %psi = 0 = -theta2-theata3-theta4+pi/2
+    theta_4=-(psi+theta_2+theta_3-(pi/2));
+    theta_5=Koordinaten(5);  
+    theta_3=-theta_3;
+    theta_2=-theta_2;
+    theta_4=-theta_4;
+end
 Winkel=[theta_1 theta_2 theta_3 theta_4 theta_5]
+
