1fee05ff69087a02fa787cc32f751d6be89cbd53
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-12 14:40:35 +0100

    code cleanup

--- Ing_YouBot/Youbot_Position.m
+++ Ing_YouBot/Youbot_Position.m
@@ -1,37 +1,37 @@
+function Y_Pos = Youbot_Position()
 
-%function Y_Pos = Youbot_Position()
+    % Abstand Marker_YB2 zu Base_YB2
+    x = 110;
 
-%GelenkPos(runROS(),[0 0 0 deg2rad(-30) 0]);
+    % Erkennen der Marker auf YB2
+    posYB2 = KreisErkennung(runROS, 'w','1',19,'Bild','Sens',0.8);
 
+    % Zerlegen und Umwandeln der Rueckgabe
+    posYB21 = posYB2(1);
+    posYB21_vektor = [posYB21.Y+280; posYB21.X; posYB21.Z+175];
+    posYB22 = posYB2(2);
+    posYB22_vektor = [posYB22.Y+280; posYB22.X; posYB22.Z+175];
 
-x = 110; %Abstand Marker zu Base
+    % Winkel zwischen Base_YB1 und Marker_YB2
+    alpha_1 = -atan2(((posYB21_vektor(2)+posYB22_vektor(2))/2),((posYB21_vektor(1)+posYB22_vektor(1))/2));
 
-Pos_Y=KreisErkennung(runROS, 'w','1',19,'Bild','Sens',0.8);
-disp(Pos_Y);
+    % halber Abstand zwischen den Markern
+    a = (1/2*norm(posYB21_vektor-posYB22_vektor));
+    % Abstand zwischen Base_YB1 und Marker_1
+    b = (norm(posYB21_vektor));
+    % Abstand zwischen Base_YB1 und Mitte Marker_YB2
+    c = (1/2*norm(posYB21_vektor+posYB22_vektor));
 
-YB21 = Pos_Y(1);
-YB22 = Pos_Y(2);
+    % Winkel zwischen Vektor c und Base_YB2  
+    alpha_2 = -(acos((a^2+c^2-b^2)/(2*a*c)) - pi/2);
 
-YB21_vektor = [YB21.Y+280; YB21.X; YB21.Z+175];
-YB22_vektor = [YB22.Y+280; YB22.X; YB22.Z+175];
+    % Distanz YB1 zu YB2
+    r = sqrt(c^2+x^2-2*x*c*cos(pi-abs(alpha_2)));
+    alpha = asin(x/r*(sin(pi-abs(alpha_2))));
 
-alpha_1 = -atan2(((YB21_vektor(2)+YB22_vektor(2))/2),((YB21_vektor(1)+YB22_vektor(1))/2));
-alp = rad2deg(alpha_1);
+    % Winkel YB1 zu YB2
+    phi = alpha+alpha_1;
 
-a = (1/2*norm(YB21_vektor-YB22_vektor));
-b = (norm(YB21_vektor));
-c = (1/2*norm(YB21_vektor+YB22_vektor));
-
-
-alpha_2 = -(acos((a^2+c^2-b^2)/(2*a*c)) - pi/2);
-alph = rad2deg(alpha_2);
-
-
-r = sqrt(c^2+x^2-2*x*c*cos(pi-abs(alpha_2)));
-alpha = asin(x/r*(sin(pi-abs(alpha_2))));
-
-%Y_Pos=[r alpha];
-
-
-                                                                                                    
-
+    % Rueckgabe r und phi
+    Y_Pos=[r phi];
+end
