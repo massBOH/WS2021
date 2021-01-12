a3a1e5a90034300791348e87541d1e0887b44b49
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-12 13:38:02 +0100

    code cleanup

--- Ing_YouBot/Winkelbegrenzung.m
+++ Ing_YouBot/Winkelbegrenzung.m
@@ -1,26 +1,29 @@
+% Funktion Winkelbegrenzung
+% prueft, ob die Winkel innerhalb der min/max-Werte liegen
+% return 0 = false, 1 = true
 
-function Winkel_test = Winkelbegrenzung(Winkel)
+function winkel_test = Winkelbegrenzung(winkel)
 
-Winkel_test = 0;
+    % Rueckgabewert
+    winkel_test = 0;
 
+    % min/max-Werte YouBot
+    theta1_min = deg2rad(-170);
+    theta1_max = deg2rad(170);
+    theta2_min = deg2rad(-65);
+    theta2_max = deg2rad(90);
+    theta3_min = deg2rad(-150);
+    theta3_max = deg2rad(150);
+    theta4_min = deg2rad(-95);
+    theta4_max = deg2rad(95);
 
-theta1_min = deg2rad(-170);
-theta1_max = deg2rad(170);
-theta2_min = deg2rad(-65);
-theta2_max = deg2rad(90);
-theta3_min = deg2rad(-150);
-theta3_max = deg2rad(150);
-theta4_min = deg2rad(-95);
-theta4_max = deg2rad(95);
-
-
-
-if (Winkel(1)>theta1_min && Winkel(1)<theta1_max)
-    if (Winkel(2)>theta2_min && Winkel(2)<theta2_max)
-        if (Winkel(3)>theta3_min && Winkel(3)<theta3_max)
-            if (Winkel(4)>theta4_min && Winkel(4)<theta4_max)
-                Winkel_test=1;
+    if (winkel(1)>=theta1_min && winkel(1)<=theta1_max)
+        if (winkel(2)>=theta2_min && winkel(2)<=theta2_max)
+            if (winkel(3)>=theta3_min && winkel(3)<=theta3_max)
+                if (winkel(4)>=theta4_min && winkel(4)<=theta4_max)
+                    winkel_test=1;
+                end
             end
         end
     end
-end    
+end
