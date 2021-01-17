65f7dbcc849672fe417cf1c84f1cee9e4e13784e
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-17 09:50:02 +0100

    min und max aus runROS uebernommen

--- Ing_YouBot/Winkelbegrenzung.m
+++ Ing_YouBot/Winkelbegrenzung.m
@@ -7,15 +7,21 @@ function winkel_test = Winkelbegrenzung(winkel)
     % Rueckgabewert
     winkel_test = 0;
 
+    % Aus runROS()
+    JointMin=[-169 -65 -151 -102 -167];
+    JointMax=[169 90 146 102 167];
+    % Angabe Veranstaltung
+    % JointMin=[-170 -65 -150 -95 -167];
+    % JointMax=[170 90 150 95 167];
     % min/max-Werte YouBot
-    theta1_min = deg2rad(-170);
-    theta1_max = deg2rad(170);
-    theta2_min = deg2rad(-65);
-    theta2_max = deg2rad(90);
-    theta3_min = deg2rad(-150);
-    theta3_max = deg2rad(150);
-    theta4_min = deg2rad(-95);
-    theta4_max = deg2rad(95);
+    theta1_min = deg2rad(JointMin(1)); 
+    theta1_max = deg2rad(JointMax(1));
+    theta2_min = deg2rad(JointMin(2));
+    theta2_max = deg2rad(JointMax(2));
+    theta3_min = deg2rad(JointMin(3));
+    theta3_max = deg2rad(JointMax(3));
+    theta4_min = deg2rad(JointMin(4));
+    theta4_max = deg2rad(JointMax(4));
 
     if (winkel(1)>=theta1_min && winkel(1)<=theta1_max)
         if (winkel(2)>=theta2_min && winkel(2)<=theta2_max)
