4c0fe31d82135499fc5ef988efb7249e22e96231
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-19 13:44:19 +0100

    Integration der Ablaufsteuerung

--- Ing_YouBot/Main.m
+++ Ing_YouBot/Main.m
@@ -1,4 +1,7 @@
 %Main
+
+% TODO: ROS Interface, Ablegen, Kommentare und Doku
+
 clear all;
 close all;
 clc;
@@ -9,35 +12,69 @@ theta2_min = deg2rad(-65);
 theta3_max = deg2rad(150);
 theta4_min = deg2rad(-95);
 
-kerzePOS = [0 0 0 0 0];
-sicherPOS = [0 deg2rad(-60) deg2rad(60) pi/2 0];
-parkPOS= [0 theta2_min theta3_max theta4_min 0];
+kerzePos = [0 0 0 0 0];
+sicherPos = [0 deg2rad(-60) deg2rad(60) pi/2 0];
+parkPos = [0 theta2_min theta3_max theta4_min 0];
 camera_youbotPOS = [0 0 0 deg2rad(-30) 0];
 camera_klotzPOS = [deg2rad(-170) 0 0 0 deg2rad(-12) 0];
 
 %Initialisierungsfahrt in die Kerze
-GelenkPos(ros,kerzePOS);
+disp('Fahre in Kerze')
+%GelenkPos(ros,kerzePOS);
 
 %Greifer öffnen
-%GreiferPos(ros, 20);
+disp('Oeffne Greifer')
+GreiferPos(ros, 20);
+
+
+yb2_nichtbereit = 0;
+disp('Pruefe YB2 bereit')
+while(~yb2_nichtbereit)
+    yb2_nichtbereit = input('Ist YB2 bereit (0 = Nein, 1 = Ja)?: ');
+    pause(0.1)
+end
+
+detect = 0;
+YB2master = 0;
+YB1master = 0;
+disp('Klotz erkennen')
+while (~detect && ~YB2master)
+    try
+        GelenkPos(ros, camera_klotzPOS);
+        klotz_Pos=Klotz_Position();
+        detect = 1;
+        YB1master = 1;
+    catch ME
+        disp("Keinen Klotz erkannt");
+        YB2master = input('Ist YB2 Master (0 = Nein, 1 = Ja)?: ');
+        pause(1);
+    end
+end
 
 %Festlegung, ob dieser Youbot übergibt oder übernimmt
-master=1;   %master=1 -> dieser Youbot übergibt
-            %master=0 -> dieser Youbot übernimmt           
-            
-%Position des Klotzes messen und anfahren, falls dieser Youbot übergibt
+if(YB1master ==1)
+    disp('Ich bin Master')
+    master=1;   % -> dieser Youbot übergibt
+else
+    disp('Ich bin Slave')
+   master=0;    % -> dieser Youbot übernimmt
+end
+
+%Position des Klotzes anfahren, falls dieser Youbot übergibt
 if master==1
-    %Klotz_aufnehmen;
-    GelenkPos(ros,kerzePOS);
+    disp('Picke Klotz')
+    Klotz_aufnehmen(ros, klotz_Pos);
+    GelenkPos(ros,kerzePos);
 end
 
 %Position des anderen Youbots messen
-nodetect = 1;
-while (nodetect)
+detect = 0;
+disp('Suche YB2')
+while (~detect)
     try
         GelenkPos(ros,camera_youbotPOS);
         Youbot_Pos=Youbot_Position();
-        nodetect = 0;
+        detect = 1;
     catch ME
         disp("YouBot2 not detected");
         pause(1);
@@ -46,8 +83,8 @@ end
 
 %Übergabeposition bestimmen
 psi = 0;
-r = Youbot_Pos(1)/2
-phi = Youbot_Pos(2)
+r = Youbot_Pos(1)/2;
+phi = Youbot_Pos(2);
 x_Uebergabe = (r)*cos(phi);
 y_Uebergabe = (r)*sin(phi);
 z_Uebergabe = Berechnung_Z(x_Uebergabe,y_Uebergabe,psi);
@@ -65,30 +102,49 @@ else
 end
 
 %Vorgabeposition anfahren
-
 %Sicherheitsabstand prüfen
-GelenkPos(ros,sicherPOS);
+GelenkPos(ros,sicherPos);
 GelenkPos(ros, Inverskinematik([x_Vorgabe y_Vorgabe z_Vorgabe 0 Theta_5]))
-GelenkPos(ros, Inverskinematik([x_Uebergabe y_Uebergabe z_Uebergabe 0 Theta_5]))
 %Trajektorien_Youbot([x_Vorgabe, y_Vorgabe, z_Uebergabe; x_Uebergabe, y_Uebergabe, z_Uebergabe],Theta_5);
+
+yb2_uebergabe = 0;
+if master==1
+    while(~yb2_uebergabe)
+        yb2_uebergabe = input('Ist YB2 in UebergabePos (0 = Nein, 1 = Ja)?: ');
+        pause(0.1)
+    end
+end
+GelenkPos(ros, Inverskinematik([x_Uebergabe y_Uebergabe z_Uebergabe 0 Theta_5]))
 
 %Übergabeposition anfahren
 %Trajektorien_Youbot([x_Uebergabe, y_Uebergabe, z_Uebergabe; x_Vorgabe, y_Vorgabe, z_Uebergabe],Theta_5);
 
 %Klotz übernehmen/übergeben
+yb2_greiferzu = 0;
 if master==1
+    while(~yb2_greiferzu)
+        yb2_greiferzu = input('Ist YB2 Greifer geschlossen? (0 = Nein, 1 = Ja)?: ');
+        pause(0.1)
+    end
     GreiferPos(runROS(), 20);
 else
+    while(~yb2_uebergabe)
+        yb2_uebergabe = input('Ist YB2 in UebergabePos (0 = Nein, 1 = Ja)?: ');
+        pause(0.1)
+    end
     GreiferPos(runROS(), 0);
 end
 
-wegdrehPOS = Inverskinematik([x_Uebergabe y_Uebergabe z_Uebergabe 0 Theta_5])
+yb2_wegdrehen = 0;
+if master==1
+    while(~yb2_wegdrehen)
+        yb2_wegdrehen = input('Ist YB2 weggedreht (0 = Nein, 1 = Ja)?: ');
+        pause(0.1)
+    end
+end
+
+wegdrehPOS = Inverskinematik([x_Uebergabe y_Uebergabe z_Uebergabe 0 Theta_5]);
 wegdrehPOS(1) = pi/4;
 GelenkPos(ros,wegdrehPOS);
-GelenkPos(ros,kerzePOS);
+GelenkPos(ros,kerzePos);
 
-%Vorgabeposition anfahren
-%Trajektorien_Youbot
-%GelenkPos(runROS(),Winkel_Vorgabe);
-
-%disp('Übergabe erfolgreich abgeschlossen')
