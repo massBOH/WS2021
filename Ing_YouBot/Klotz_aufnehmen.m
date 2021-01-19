ec21f461e66056b7eb80e2db82f9b5d1eee333aa
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-19 13:40:32 +0100

    Umbau zu Funktion

--- Ing_YouBot/Klotz_aufnehmen.m
+++ Ing_YouBot/Klotz_aufnehmen.m
@@ -1,34 +1,31 @@
-% Anfahren Kameraposition
-GelenkPos(ros,camera_klotzPos);
+function Klotz_aufnehmen(ROS, klotzPos)
+    kerzePos = [0 0 0 0 0];
+    % Hoehenfsets
+    zPre = 5;
+    zPick = -20;
 
-% Klotzposition bestimmen
-klotzPos=Klotz_Position();
+    % Inverskinematik der Positionen
+    prePickup = Inverskinematik([klotzPos(1) klotzPos(2) klotzPos(3)+zPre -pi/2 0]);
+    pickup = Inverskinematik([klotzPos(1) klotzPos(2) klotzPos(3)+zPick -pi/2 0]);
 
-% Hoehenoffsets
-zPre = 5
-zPick = -20
-
-% Inverskinematik der Positionen
-prePickup = Inverskinematik([klotzPos(1) klotzPos(2) klotzPos(3)+zPre -pi/2 0]);
-pickup = Inverskinematik([klotzPos(1) klotzPos(2) klotzPos(3)+zPick -pi/2 0]);
-
-% Pruefung IK gueltig
-if Winkelbegrenzung(prePickup)
-    if Winkelbegrenzung(pickup)
-        % Anfahren Kerze
-        GelenkPos(ros,kerzePos);
-        % Anfahren prePickup
-        GelenkPos(ros,prePickup);
-        % Trajektorie Pickup
-        GelenkPos(ros,pickup);
-        GreiferPos(ros, 0);
-        % Trajektorie prePickup
-        GelenkPos(ros,prePickup);
-        % Anfahren Kerze
-        GelenkPos(ros,kerzePos);
+    % Pruefung IK gueltig
+    if Winkelbegrenzung(prePickup)
+        if Winkelbegrenzung(pickup)
+            % Anfahren Kerze
+            GelenkPos(ROS, kerzePos);
+            % Anfahren prePickup
+            GelenkPos(ROS, prePickup);
+            % Trajektorie Pickup
+            GelenkPos(ROS, pickup);
+            GreiferPos(ROS, 0);
+            % Trajektorie prePickup
+            GelenkPos(ROS, prePickup);
+            % Anfahren Kerze
+            GelenkPos(ROS, kerzePos);
+        else
+            error('Winkelbegrenzung pickup überschritten');
+        end
     else
-        error('Winkelbegrenzung pickup überschritten');
+        error('Winkelbegrenzung prePick überschritten');
     end
-else
-    error('Winkelbegrenzung prePick überschritten');
 end
