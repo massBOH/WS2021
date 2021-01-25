87b40cd9b672f326075f53490127c16e709c1120
GeraldHebinck <43569784+GeraldHebinck@users.noreply.github.com>
2021-01-25 18:12:56 +0100

    Umbau zu Funktion

--- Ing_YouBot/Trajektorien_Youbot.m
+++ Ing_YouBot/Trajektorien_Youbot.m
@@ -1,28 +1,27 @@
-%Trajektorien_Youbot(Punkte, Theta_5)
-% createSerialLink_zeroModify;
-%Darstellung Verbindungsvektor linear
-%Einlesen Startpunkt - Endpunkt
+function Trajektorien_Youbot(ROS, Punkte, Theta_5)
+    %Einlesen Startpunkt - Endpunkt
 
-p1 = [Punkte(1,1) Punkte(1,2) Punkte(1,3)];
-p2 = [Punkte(2,1) Punkte(2,2) Punkte(2,3)];
+    p1 = [Punkte(1,1) Punkte(1,2) Punkte(1,3)];
+    p2 = [Punkte(2,1) Punkte(2,2) Punkte(2,3)];
 
-P=[p1; p2];
+    P=[p1; p2];
 
-%Berechnung der Stützpunkte
-a = 0 : 0.25 : 1;
-a = transpose(a);
-steps=size(a);
-n=size(P);
-for i=1:n-1
-    X = a*[P(i+1, :)-P(i,:)]+P(i,:);
-    Trajec_Array(:,:,i)=X;
-end
+    %Berechnung der Stützpunkte
+    a = 0 : 0.25 : 1;
+    a = transpose(a);
+    steps=size(a);
+    n=size(P);
+    for i=1:n-1
+        X = a*[P(i+1, :)-P(i,:)]+P(i,:);
+        Trajec_Array(:,:,i)=X;
+    end
 
-for z=1:n-1
-    for s =1:steps
-        Arbeitsraum_fertig([Trajec_Array(s,1,z) Trajec_Array(s,2,z) Trajec_Array(s,3,z) 0 Theta_5]);
-        Winkel=Inverskinematik_fertig([Trajec_Array(s,1,z) Trajec_Array(s,2,z) Trajec_Array(s,3,z) 0 Theta_5]);
-        GelenkPos(runROS(),Winkel);
-        pause(0.1);
+    for z=1:n-1
+        for s =1:steps
+            Arbeitsraum_fertig([Trajec_Array(s,1,z) Trajec_Array(s,2,z) Trajec_Array(s,3,z) 0 Theta_5]);
+            Winkel=Inverskinematik_fertig([Trajec_Array(s,1,z) Trajec_Array(s,2,z) Trajec_Array(s,3,z) 0 Theta_5]);
+            GelenkPos(ROS,Winkel);
+            pause(0.1);
+        end
     end
 end
