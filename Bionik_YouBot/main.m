43b002d3b6949b4c4123a0af9b62da63e26e322f
alina <alina@alina-asus>
2021-02-24 18:32:22 +0100

    Comments

--- Bionik_YouBot/main.m
+++ Bionik_YouBot/main.m
@@ -1,37 +1,40 @@
+% RSY 2020/21
+% T. Thuilot und A. Heid
+
+% Main Funktion
+% zusätzlich zu starten sind:
+% roscore
+% roslaunch youbot_driver_ros_interface youbot_driver.launch            % Hardware
+% roslaunch astra_launch astra.launch                                   % Tiefenkamera
+% /usr/local/MATLAB/R2018b/bin/matlab -nodesktop -nosplash -r Hermes    % Datenaustausch
+
+
+% Initialisieren von ROS
 rosshutdown
 
 rosinit
 
-% aktiv = 1;
-% master = 2;
-% posi = 4;
-% gripperZu = 8;
-% gedreht = 16;
-% ACK = 32;
+% Variablen definieren
+active_int      = 1; % ist youbot bereit?
+master_int      = 2; % ist youbot master?
+ubergabe_int    = 4; % ist youbot in erwarteter Position ?
+gripperz_int    = 8; % default = zu
 
-standby_int= 0;
-active_int = 1;
-master_int = 2;
-ubergabe_int  = 4;
-gripperz_int= 8; %zu
-% gedreht = 16;
-% ACK = 32;
-
-
-Publisher=rospublisher('/YB1','std_msgs/Byte'); % Publisher für Hermes
-Nachricht=rosmessage(Publisher);%Nachricht initialisieren
-Subscriber=rossubscriber('/YB2','std_msgs/Byte'); % Subscriber für Hermes
+% publisher und subscriber festlegen
+Publisher = rospublisher('/YB1','std_msgs/Byte'); % Publisher für Hermes
+Nachricht = rosmessage(Publisher);%Nachricht initialisieren
+Subscriber = rossubscriber('/YB2','std_msgs/Byte'); % Subscriber für Hermes
 
 % check if ready
-lauschen=1;
+lauschen = 1;
 while lauschen == 1
-        Nachricht.Data =active_int;
+        Nachricht.Data = active_int;
         send(Publisher,Nachricht);
         disp("bereit und warte")
         
         try
            NaYB2 = receive(Subscriber, 0.1);
-           if NaYB2.Data==active_int
+           if NaYB2.Data == active_int
                lauschen = 0;
            end
 
@@ -41,39 +44,32 @@ end
 
 
 disp('Beide Bots bereit')
-master = 2
+master = 2;
 
+% wird ein Klotz erkannt?
 while master == 2
-    
-    
-    klotz_arr_ = KlotzPositionErkennen()
-
+    klotz_arr_ = KlotzPositionErkennen(); % versuchen einen Klotz zu erkennen
     disp(klotz_arr_)
-
-    if isnan(klotz_arr_)
-
+    if isnan(klotz_arr_) % wenn kein Klotz erkannt wurde
             disp('gibts master?')
             try
-               NaYB2 = receive(Subscriber, 0.1);
-               if NaYB2.Data==master_int
+               NaYB2 = receive(Subscriber, 0.1); % lauschen, ob anderer Youbot master ist
+               if NaYB2.Data == master_int % wenn anderer youbot Master ist, sind wir slave
                    master = 0;
                     disp("slave")
                     GreiferPos(runROS(),20); % Greifer offen
-
                end
 
             catch
                 continue
             end
-
         
-        
-    else
+    else % wenn ein Klotz gefunden wurde
 
-        master = 1;
+        master = 1; % wir sind master
         Nachricht.Data = master_int;
-        send(Publisher,Nachricht); 
-        KlotzPositionAnfahren(klotz_arr_);
+        send(Publisher,Nachricht); % Nachricht wird an anderen youbot gesendet
+        KlotzPositionAnfahren(klotz_arr_); % KLotz wird angefahren und aufgenommen
         disp("master")
     
     end
@@ -82,23 +78,21 @@ while master == 2
 end
 
 
-
-
-
-for i = 1:20
+for i = 1:20 % mehrmals versuchen anderen youbot zu erkennen
     try
         erkannte_uebergabe = 0;
-        erkannte_uebergabe = ubergabe_erkennung();
-        if erkannte_uebergabe ~= 0
+        erkannte_uebergabe = ubergabe_erkennung(); % versuchen anderen youbot zu erkennen 
+        if erkannte_uebergabe ~= 0 % wenn die Position des anderen youbots erkannt wurde
          break
         end
     catch
-        disp("LICHT")
+        disp("LICHT") % anderer youbot wird häufig nicht erkannt, weil es zu wenig Licht gibt
         continue
     end
 
 end
 
+% (vor-)übergabe Positionen festlegen
 uebergabeX    = erkannte_uebergabe(1);
 uebergabeY    = erkannte_uebergabe(2);
 uebergabeZ    = erkannte_uebergabe(3);
@@ -106,10 +100,11 @@ voruebergabeX = erkannte_uebergabe(4);
 voruebergabeY = erkannte_uebergabe(5);
 voruebergabeZ = erkannte_uebergabe(3);
 
+% Greifer horizontal oder vertikal drehen je nach master oder slave
 if master == 1
     theta5 = pi/2;
     
-    Nachricht.Data =master_int;
+    Nachricht.Data = master_int;
     send(Publisher,Nachricht);
 
     
@@ -117,18 +112,20 @@ else
     theta5 = 0;
 end
 
+% inverse Kinematik für (vor-)übergabe Positionen berechnen
 voruebergabe = inverse_youbot_real([voruebergabeX voruebergabeY voruebergabeZ 0 theta5]); 
 uebergabe = inverse_youbot_real([uebergabeX uebergabeY uebergabeZ 0 theta5]); 
 
 GelenkPos(runROS,voruebergabe); %voruebergabe position
-%wenn master -> warte das slave in übergabe pos
+
+% wenn master -> warte das slave in der Übergabeposition ist
 if master ==1
-    lauschen=1;
+    lauschen = 1;
     while lauschen == 1
             disp('Ist Slave in Ubergabe?')
             try
                NaYB2 = receive(Subscriber, 0.1);
-               if NaYB2.Data==ubergabe_int
+               if NaYB2.Data == ubergabe_int
                    lauschen = 0;
                end
 
@@ -137,23 +134,23 @@ if master ==1
     end
     
 end
-GelenkPos(runROS,uebergabe); %uebergabe position
-% wenn slave -> sende das ubergabeposition erreicht
+
+GelenkPos(runROS,uebergabe); % Übergabeposition
+
+% wenn slave -> sende das Übergabeposition erreicht
 if master == 0
-   Nachricht.Data =ubergabe_int;
+   Nachricht.Data = ubergabe_int;
    send(Publisher,Nachricht); 
 end
 
-%wartet das slave
-%greifer schließt
-%master oeffnet greifer -> sendet das greifer offen ist 
+
 if master == 1
-    lauschen=1;
-    while lauschen == 1
+    lauschen = 1;
+    while lauschen == 1 % wartet das slave den Greifer schließt
             disp('Ist der Gripper von Slave zu?')
             try
                NaYB2 = receive(Subscriber, 0.1);
-               if NaYB2.Data==gripperz_int
+               if NaYB2.Data == gripperz_int
                    lauschen = 0;
                end
 
@@ -161,16 +158,16 @@ if master == 1
             end
     end 
     GreiferPos(runROS(),20); % Greifer offen
-else 
-    lauschen=1;
+else  % wenn slave, wartet, dass mater 
+    lauschen = 1;
     while lauschen == 1
         disp('Ist Master in Ubergabe?')
             try
-               Nachricht.Data =ubergabe_int;
+               Nachricht.Data = ubergabe_int;
                send(Publisher,Nachricht);
                NaYB2 = receive(Subscriber, 0.1);
-               if NaYB2.Data==ubergabe_int
-                   Nachricht.Data =32;
+               if NaYB2.Data == ubergabe_int
+                   Nachricht.Data = 32;
                    send(Publisher,Nachricht);
                    lauschen = 0;
                end
@@ -179,13 +176,13 @@ else
             end
     end 
     GreiferPos(runROS(),0); % Greifer schließen
-    %% noch warten auf greifer offen
-    lauschen=1;
+    % noch warten auf geöffneten Greifer des Masters
+    lauschen = 1;
     while lauschen == 1
-        disp('Ist Master offen?')
+        disp('Ist Greifer von Master offen?')
             try
                NaYB2 = receive(Subscriber, 0.1);
-               if NaYB2.Data==gripperz_int
+               if NaYB2.Data == gripperz_int
                    lauschen = 0;
                end
 
@@ -196,20 +193,20 @@ else
     
 end
 
-
+% aus Übergabeposition mit Sicherheitsrotation bewegen
 safe_rot = [pi/4 0 0 0 0];
-GelenkPos(runROS,uebergabe+safe_rot); %voruebergabe position
+GelenkPos(runROS,uebergabe+safe_rot); % Voruebergabe position
 
 
 
 if master == 1
-    GelenkPos(runROS,[0, 0, 0, 0, 0]);
+    GelenkPos(runROS,[0, 0, 0, 0, 0]); % wenn slave, in Kerzenposition
 else 
-    GelenkPos(runROS,[-0.1877 ,  -0.7055 ,  -0.6348 ,  -1.8013   ,      0]); %ablage mit sicherheit
-    GelenkPos(runROS,[ -0.1876 ,  -0.6757 ,  -0.9024 ,  -1.5635 ,0]);
+    GelenkPos(runROS,[-0.1877 ,  -0.7055 ,  -0.6348 ,  -1.8013   ,      0]); % Ablage mit Sicherheit
+    GelenkPos(runROS,[ -0.1876 ,  -0.6757 ,  -0.9024 ,  -1.5635 ,0]); % Ablageposition für den Klotz
     GreiferPos(runROS(),20); % Greifer offen
-    GelenkPos(runROS,[-0.1877 ,  -0.7055 ,  -0.6348 ,  -1.8013   ,      0]); %ablage mit sicherheit
-    GelenkPos(runROS,[0, 0, 0, 0, 0]);
+    GelenkPos(runROS,[-0.1877 ,  -0.7055 ,  -0.6348 ,  -1.8013   ,      0]); % Ablage mit Sicherheit
+    GelenkPos(runROS,[0, 0, 0, 0, 0]); % Kerze
 end
 
          
